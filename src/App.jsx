dc20fd1f5523952db46a6c9025d796d5a9626ab8
abdulahaddf <abdulahad.df1@gmail.com>
2024-04-07 14:12:34 +0600

    home page updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import DemoForm from "./pages/Home/DemoForm";
+import DataInput from "./pages/Home/DataInput";
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={<HomePage />} path="/" exact />
+        <Route element={<DataInput/>} path="/add-phone-info" />
         <Route element={<DemoForm/>} path="/demo" exact />
       </Routes>
     </>
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import DataInput from './DataInput';
-import DemoForm from './DemoForm';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div>
-            {/* <DemoForm/> */}
-            <DataInput/>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-5xl mb-20">WELCOME TO TECH HIVE</h1>
+      <div>
+
+      <Link
+        to="view-phone-info"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+        View List
+      </Link>
+      <Link
+        to="add-phone-info"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+        Add Phone
+      </Link>
+          </div>
+    </div>
+  );
 };
 
 export default HomePage;
