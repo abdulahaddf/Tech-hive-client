b15a618dd97b7ddfaf249679f108c6f7c332376f
abdulahaddf <abdulahad.df1@gmail.com>
2024-04-07 14:21:22 +0600

    home page view list

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -5,21 +5,20 @@ const HomePage = () => {
   return (
     <div>
       <h1 className="text-5xl mb-20">WELCOME TO TECH HIVE</h1>
-      <div>
-
-      <Link
-        to="view-phone-info"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      <div className="flex gap-10 justify-center">
+        <Link
+          to="view-phone-info"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-        View List
-      </Link>
-      <Link
-        to="add-phone-info"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          View List
+        </Link>
+        <Link
+          to="add-phone-info"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-        Add Phone
-      </Link>
-          </div>
+          Add Phone
+        </Link>
+      </div>
     </div>
   );
 };
---
+++ src/pages/View/ViewList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewList = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ViewList;
